09bb1f81340c6895741c164db584ec61e6f49d82
Gervasio Junior <juniorb08@gmail.com>
2015-07-18 02:23:48 -0300

    atualizando script

--- script/init.sh
+++ script/init.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y wget apache2 libapache2-mod-php5
+sudo apt-get install -y git wget apache2 libapache2-mod-php5
 sudo a2enmod php5
 sudo service apache2 restart
 
-cd /var/www
-sudo rm index.html
-sudo wget https://raw.githubusercontent.com/EnioAbrantes/Atividade-AWS/master/html/index.html
-sudo wget https://raw.githubusercontent.com/EnioAbrantes/Atividade-AWS/master/html/style.css
+cd /var/www/html
+sudo rm *
+
+sudo git clone https://github.com/EnioAbrantes/Atividade-AWS.git 
+sudo mv ./Atividade-AWS/* ./
