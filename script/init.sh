efd8ae1e945c0bed5a0cc23ea07966e87ebf4c61
gervasiocaj <juniorb08@gmail.com>
2015-07-18 12:41:39 -0300

    Update init.sh

--- script/init.sh
+++ script/init.sh
@@ -4,8 +4,6 @@ sudo apt-get install -y git wget apache2 libapache2-mod-php5
 sudo a2enmod php5
 sudo service apache2 restart
 
-cd /var/www/html
-sudo rm *
-
-sudo git clone https://github.com/EnioAbrantes/Atividade-AWS.git 
-sudo mv ./Atividade-AWS/* ./
+# o diretorio html/ dentro de /var/www/ sera substituido pelo html/ do repositorio
+sudo rm -r /var/www/*
+sudo git clone https://github.com/EnioAbrantes/Atividade-AWS.git /var/www/
